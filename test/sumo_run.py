a60ca086e4ff72077e6896ba6a131cc52bfe22db
Rohak Roy <Rohak.Roy@toshiba-bril.com>
2023-03-23 13:37:08 +0000

    added a test folder for getting used to TraCI

---
+++ test/sumo_run.py
@@ -0,0 +1,40 @@
+import traci
+import time
+import traci.constants as tc
+import pytz
+import datetime
+from random import randrange
+import os
+import sys
+from sumolib import checkBinary
+import optparse
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+def getDateTime():
+    utc_now = pytz.utc.localize(datetime.datetime.utcnow())
+    currentDT = utc_now.astimezone(pytz.timezone("GB"))
+    DATIME = currentDT.strftime("%Y-%m-%d %H:%M:%S")
+    return DATIME
+
+sumoCmd = ["sumo-gui", "-c", "data\experimenting.sumocfg"]
+traci.start(sumoCmd)
+
+counter = 0
+while traci.simulation.getMinExpectedNumber() > 0:
+
+    traci.simulationStep()
+    counter += 1
+
+    # vehicles = traci.vehicle.getIDList()
+
+    # print("Iteration", counter, " = ", vehicles)
+
+    vehicles = traci.edge.getLastStepVehicleIDs("WtoJ")
+    print("Iteration", counter, " = ", vehicles)
+
+traci.close()
